59646a7a14cb7e149f6528ed68eb571870d30dd7
Puneet Barhaiya <92726042+Puneet-Barhaiya@users.noreply.github.com>
2026-03-16 12:54:29 +0530

    Create Hero.js

---
+++ Hero.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Hero(){
+
+return(
+
+<section className="hero">
+
+<h1>Hire The Best Talent</h1>
+
+<p>India's fastest growing recruitment consultancy</p>
+
+<button>Contact Us</button>
+
+</section>
+
+)
+
+}
+
+export default Hero
