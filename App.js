dec8fea625e4333545fcafed2c0d7a0beb68fecd
Puneet Barhaiya <92726042+Puneet-Barhaiya@users.noreply.github.com>
2026-03-16 12:51:32 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+
+      <Navbar />
+
+      <Hero />
+
+      <Services />
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
